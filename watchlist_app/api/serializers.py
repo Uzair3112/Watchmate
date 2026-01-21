2328b3a21964773ebd0cae449c7ed329d72a1341
Uzair3112 <uzairtariq.pakistani@gmail.com>
2026-01-21 17:13:31 +0500

    Added username for review

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,22 +2,24 @@ from rest_framework import serializers
 from watchlist_app.models import Watchlist, StreamPlatform, Review
 
 class ReviewSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Review
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ('watchlist',)
 
 
 class WatchlistSerializer(serializers.ModelSerializer):
-    #reviews = ReviewSerializer(many=True, read_only=True)
-    reviews = serializers.StringRelatedField(many=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
+    # reviews = serializers.StringRelatedField(many=True)
     class Meta:
         model = Watchlist
         fields = '__all__'
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
-    #watchlist = WatchlistSerializer(many=True, read_only=True)
-    watchlist = serializers.StringRelatedField(many=True, read_only=True)
+    watchlist = WatchlistSerializer(many=True, read_only=True)
+    # watchlist = serializers.StringRelatedField(many=True, read_only=True)
     # watchlist = serializers.HyperlinkedRelatedField(
     #     many=True, read_only=True,
     #     view_name='movie-detail')
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from watchlist_app.models import Watchlist, StreamPlatform, Review
 from watchlist_app.api.serializers import WatchlistSerializer, StreamPlatformSerializer, ReviewSerializer
 from rest_framework.response import Response
@@ -14,11 +15,18 @@ class StreamPlatformVS(viewsets.ModelViewSet):
 
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+    
+    def get_queryset(self):
+        return Review.objects.all()
 
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         movie = Watchlist.objects.get(pk=pk)
-        serializer.save(watchlist=movie)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!")
+        serializer.save(watchlist=movie, review_user=review_user)
 
 class ReviewList(generics.ListAPIView):
     # queryset = Review.objects.all()
