217fc4457e703a35ccf51b840869c02cde1ccf51
Uzair3112 <uzairtariq.pakistani@gmail.com>
2026-01-24 16:36:15 +0500

    User authentication using auth token

---
+++ user_app/api/views.py
@@ -0,0 +1,25 @@
+from rest_framework.decorators import api_view
+from user_app.api.serializers import RegistrationSerializer
+from user_app.models import create_auth_token
+
+import rest_framework.status as status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+
+
+@api_view(['POST',])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = "Registration Successful"
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data = serializer.errors
+        return Response(data, status=status.HTTP_201_CREATED)
