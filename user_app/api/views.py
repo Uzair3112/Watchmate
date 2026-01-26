c200529e8791ab20ec420005af31baf450f143fd
Uzair3112 <uzairtariq.pakistani@gmail.com>
2026-01-26 19:55:51 +0500

    Remove auth token by logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -7,6 +7,12 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 
 
+@api_view(['POST',])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
 
 @api_view(['POST',])
 def registration_view(request):
